4b1e93a9759ae7147950f48c58c6d343fbd793d5
Vincenzo <vinzoffoli@gmail.com>
2026-05-15 07:46:02 -0600

    widget

--- components/OpenTableWidget.tsx
+++ components/OpenTableWidget.tsx
@@ -14,7 +14,7 @@ export default function OpenTableWidget() {
     s.type = 'text/javascript';
     // Explicit https:// so it matches the CSP allowlist (// resolves to http:// on localhost)
     s.src =
-      'https://www.opentable.com/widget/reservation/loader?rid=72262&type=standard&theme=standard&color=1&dark=true&iframe=false&domain=com&lang=en-US&newtab=false&ot_source=Restaurant%20website&cfe=true';
+      'https://www.opentable.com/widget/reservation/loader?rid=72262&type=standard&theme=standard&color=8&dark=false&iframe=true&domain=com&lang=en-US&newtab=false&ot_source=Restaurant%20website&font=brandonText&ot_logo=subtle&primary_color=ffffff&primary_font_color=333333&button_color=C69C6D&button_font_color=333333&logo_pid=84578883&cfe=true';
     ref.current.appendChild(s);
   }, []);
 
